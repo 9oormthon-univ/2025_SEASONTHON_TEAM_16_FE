79d09b6767d827a27ecfeb3546b7eff9b9fd167f
qkrxogmla <suk05553@naver.com>
2025-09-07 02:57:32 +0900

    feat: 커뮤니티 best

--- yein/src/routes/AppRoutes.jsx
+++ yein/src/routes/AppRoutes.jsx
@@ -6,6 +6,8 @@ import PetPage from "../pages/Pet/PetPage";
 import TranscriptionPage from "../pages/Transcription/TranscriptionPage";
 import GalleryPage from "../pages/Gallery/GalleryPage";
 import CommunityPage from "../pages/Community/CommunityPage";
+import MyPostsPage from "../pages/Community/MyPostsPage";
+import PostCreatePage from "../pages/Community/PostCreatePage";
 import LoginSuccess from "../pages/Login/LoginSuccess";
 import ProfilePage from "../pages/Profile/ProfilePage";
 import AnalyzePage from "../pages/Analyze/AnalyzePage";
@@ -15,20 +17,32 @@ import RankPage from "../pages/Gallery/RankPage";
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* 인증 관련 */}
       <Route path="/" element={<Signup />} />
       <Route path="/login" element={<Signup />} />
       <Route path="/login/success" element={<LoginSuccess />} />
       <Route path="/oidc-callback" element={<Redirection />} />
 
+      {/* 메인 페이지 */}
       <Route path="/home" element={<HomePage />} />
       <Route path="/pet" element={<PetPage />} />
       <Route path="/transcription" element={<TranscriptionPage />} />
+
+      {/* 갤러리 */}
       <Route path="/gallery" element={<GalleryPage />} />
-      <Route path="/community" element={<CommunityPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
       <Route path="/detail/:galleryId" element={<DetailPage />} />
       <Route path="/analyze" element={<AnalyzePage />} />
+
+      {/* 랭크 */}
       <Route path="/rank" element={<RankPage />} />
+
+      {/* 커뮤니티 */}
+      <Route path="/community" element={<CommunityPage />} />
+      <Route path="/community/myposts" element={<MyPostsPage />} />
+      <Route path="/community/write" element={<PostCreatePage />} />
+
+      {/* 프로필 */}
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   );
 }
